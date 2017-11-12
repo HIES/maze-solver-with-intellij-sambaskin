674b0cd20db612a3236255ac0465cf42d6f93706
Same Basket <30935970+sambaskin@users.noreply.github.com>
2017-11-12 11:54:33 -0500

    Working solver (no extensions)

--- Maze.java
+++ Maze.java
@@ -1,9 +1,9 @@
-public class Maze
-{
+
+public class Maze {
     private Cell[][] board;
     private final int DELAY = 200;
 
-    public Maze(int rows, int cols, int[][] map){
+    public Maze(int rows, int cols, int[][] map) {
         StdDraw.setXscale(0, cols);
         StdDraw.setYscale(0, rows);
         board = new Cell[rows][cols];
@@ -11,40 +11,84 @@ public class Maze
         int height = board.length - 1;
         for (int r = 0; r < rows; r++)
             for (int c = 0; c < cols; c++) {
-                board[r][c] = map[r][c] == 1 ? new Cell(c , height - r, 0.5, false) : new Cell(c, height - r, 0.5, true);
+                board[r][c] = map[r][c] == 1 ? new Cell(c, height - r, 0.5, false) : new Cell(c, height - r, 0.5, true);
             }
     }
 
-    public void draw()
-    {
+    public void draw() {
         for (int r = 0; r < board.length; r++)
-            for (int c = 0; c < board[r].length; c++){
+            for (int c = 0; c < board[r].length; c++) {
                 Cell cell = board[r][c];
                 StdDraw.setPenColor(cell.getColor());
                 StdDraw.filledSquare(cell.getX(), cell.getY(), cell.getRadius());
             }
-            StdDraw.show();
+        StdDraw.show();
     }
 
-    public boolean findPath(int row, int col)
-    {
+    public boolean findPath(int row, int col) {
         boolean isFinished = false;
+        if (isValid(row, col)) {
+            board[row][col].visitCell();
+            this.draw();
+            StdDraw.pause(DELAY);
+
+            if (isExit(row, col)) {
+
+                board[row][col].becomePath();
+                isFinished = true;
+
+            }
+
+            if(!isFinished) {
+
+            if (!isFinished) {
+
+                isFinished = findPath(row + 1, col);
+                System.out.println("1");
+            }
+
+            if (!isFinished) {
+
+                isFinished = findPath(row - 1, col);
+                System.out.println("2");
+            }
+
+
+            if (!isFinished) {
+
+                isFinished = findPath(row, col + 1);
+                System.out.println("3");
+            }
+
+            if (!isFinished) {
+
+                isFinished = findPath(row, col - 1);
+                System.out.println("4");
+            }
+            if(isFinished) {
+                board[row][col].becomePath();
+            }
+
+
+
+                }
+
+        }
 
         return isFinished;
     }
 
-    private boolean isValid(int row, int col)
-    {
-        if(row>=0 && col>= 0 && board[row][col].isWall()==false && row < board.length && col < board[0].length)
+
+    private boolean isValid(int row, int col) {
+        if (row >= 0 && col >= 0 && row < board.length && col < board[0].length && board[row][col].isWall() == false
+                && !board[row][col].isVisited())
             return true;
 
         return false;
     }
 
-    private boolean isExit(int row, int col)
-    {
-        if(row==board.length-1 && col == board[0].length-1)
-        {
+    private boolean isExit(int row, int col) {
+        if (row == board.length - 1 && col == board[0].length - 1) {
             return true;
         }
         return false;
@@ -52,16 +96,16 @@ public class Maze
 
     public static void main(String[] args) {
         StdDraw.enableDoubleBuffering();
-        int[][] maze = {{1,1,0,0,0,0,0,0,0,0},
-                        {0,1,1,1,1,0,1,1,1,0},
-                        {0,1,1,1,1,0,1,1,0,0},
-                        {0,1,0,1,1,1,1,1,1,0},
-                        {0,1,0,0,0,0,0,1,1,0},
-                        {0,1,1,0,1,1,1,1,1,0},
-                        {0,0,1,0,0,1,0,1,0,0},
-                        {0,1,1,0,1,1,0,1,1,0},
-                        {0,1,1,0,1,1,0,1,1,0},
-                        {0,0,0,0,0,0,0,0,1,1}};
+        int[][] maze = {{1, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 1, 1, 1, 1, 0, 1, 1, 1, 0},
+                {0, 1, 1, 1, 1, 0, 1, 1, 0, 0},
+                {0, 1, 0, 1, 1, 1, 1, 1, 1, 0},
+                {0, 1, 0, 0, 0, 0, 0, 1, 1, 0},
+                {0, 1, 1, 0, 1, 1, 1, 1, 1, 0},
+                {0, 0, 1, 0, 0, 1, 0, 0, 0, 0},
+                {0, 1, 1, 0, 1, 1, 0, 1, 1, 0},
+                {0, 1, 1, 0, 1, 1, 0, 1, 1, 0},
+                {0, 0, 0, 0, 0, 1, 1, 1, 1, 1}};
         Maze geerid = new Maze(maze.length, maze[0].length, maze);
         geerid.draw();
         geerid.findPath(0, 0);
