1843e362acd9eb6c0a52cde4700b55745b64eff6
Same Basket <30935970+sambaskin@users.noreply.github.com>
2017-11-12 15:15:16 -0500

    FINAL SUBMISSION
    Includes the cell marker extension and two different versions of a random maze creation extension.
    One conventionally makes a random maze while the other makes a more "natural" looking maze. 
    (Comment and uncomment in main method to run either the hardcoded, random, or natural-random array)

--- Maze.java
+++ Maze.java
@@ -1,16 +1,16 @@
 import sun.plugin2.util.ColorUtil;
-
+//In main Args: Enter maze, mazeGen, or mazeGenNatural
 import java.awt.*;
 
 public class Maze {
     private Cell[][] board;
-    private final int DELAY = 200;
+    private final int DELAY = 20;
 
 
     private int prevCol = 0;    //Previous column modified for reverting color
     private int prevRow = 0;    //Previous row modified for reverting color
 
-    Color cellMarker = new Color(250, 80, 80);  //New color to mark cells
+    Color cellMarker = new Color(150, 0, 90);  //New color to mark cells
 
     public Maze(int rows, int cols, int[][] map) {
         StdDraw.setXscale(0, cols);
@@ -22,6 +22,85 @@ public class Maze {
             for (int c = 0; c < cols; c++) {
                 board[r][c] = map[r][c] == 1 ? new Cell(c, height - r, 0.5, false) : new Cell(c, height - r, 0.5, true);
             }
+    }
+
+    public static int[][] generateMaze(int squareLength, double p) {
+        int[][] newMaze = new int[squareLength][squareLength];
+
+        for (int r = 0; r < newMaze.length; r++) {
+            for (int c = 0; c < newMaze.length; c++) {
+                double rand = Math.random();
+                if (r == newMaze.length - 1 && c == newMaze.length - 1 || r == newMaze.length - 1 && c == newMaze.length - 2
+                        || r == 0 && c == 0 || r == 0 && c == 1) //Checks if cell is start or exit
+                {
+                    newMaze[r][c] = 1;
+                } else if (r == 0 || c == 0 || r == newMaze.length - 1 || c == newMaze.length - 1) //Check if cell is border
+                {
+                    newMaze[r][c] = 0;
+                } else {
+                    if (rand > p)
+                        newMaze[r][c] = 1;
+                    else
+                        newMaze[r][c] = 0;
+                }
+            }
+        }
+        return newMaze;
+    }
+
+
+    public static int[][] generateMazeNatural(int squareLength, double p, int strands) {
+        int[][] newMaze = new int[squareLength][squareLength];
+        {
+            int direction = 0; //0-left 1-up 2-right 3-down
+            for (int r = 0; r < newMaze.length; r++) {      //Loop fills in borders with walls and the rest with open
+                for (int c = 0; c < newMaze.length; c++) {
+                    if (r == 0 || c == 0 || r == newMaze.length - 1 || c == newMaze.length - 1) {
+                        newMaze[r][c] = 0;
+                    } else {
+                        newMaze[r][c] = 1;
+                    }
+                    if (r == newMaze.length - 1 && c == newMaze.length - 1 || r == newMaze.length - 1 && c == newMaze.length - 2
+                            || r == 0 && c == 0 || r == 0 && c == 1) //Checks if cell is start or exit
+                    {
+                        newMaze[r][c] = 1;
+                    }
+
+                }
+            }
+
+            while (strands > 0) {
+                strands--;
+
+                int timer = (int) (Math.random() * squareLength*.4);
+                int r = (int) (Math.random() * squareLength);
+                int c = (int) (Math.random() * squareLength);
+                while (timer > 0) {
+                    double changeDir = Math.random();
+                    if (changeDir > .75) {
+                        direction = (int) (4 * Math.random());
+                    }
+                    //random start. Moves in direction and changes. random lenth
+                    double rand = Math.random();
+                    if (r > 0 && c > 0 && r < squareLength - 1 && c < squareLength - 1) {
+                        newMaze[r][c] = 0;
+                        if (direction == 0)
+                            r++;
+                        if (direction == 1)
+                            r--;
+                        if (direction == 2)
+                            c++;
+                        if (direction == 3)
+                            c--;
+
+                    }
+                    timer--;
+                }
+            }
+
+        }
+
+        return newMaze;
     }
 
 
@@ -53,7 +132,7 @@ public class Maze {
 
             }
 
-            if(!isFinished) {
+            if (!isFinished) {
 
 
                 if (!isFinished) {
@@ -63,32 +142,29 @@ public class Maze {
                 }
                 if (!isFinished) {
 
-                    isFinished = findPath(row, col - 1);
-                    System.out.println("4");
-                }
-                if (!isFinished) {
-
                     isFinished = findPath(row + 1, col);
                     System.out.println("1");
                 }
+                if (!isFinished) {
 
-            if (!isFinished) {
-
-                isFinished = findPath(row - 1, col);
-                System.out.println("2");
-            }
-
-
-
-
-
-            if(isFinished) {
-                board[row][col].becomePath();
-            }
-
-
-
+                    isFinished = findPath(row, col - 1);
+                    System.out.println("4");
                 }
+
+
+                if (!isFinished) {
+
+                    isFinished = findPath(row - 1, col);
+                    System.out.println("2");
+                }
+
+
+                if (isFinished) {
+                    board[row][col].becomePath();
+                }
+
+
+            }
 
         }
 
@@ -112,6 +188,7 @@ public class Maze {
     }
 
     public static void main(String[] args) {
+
         StdDraw.enableDoubleBuffering();
         int[][] maze = {{1, 1, 0, 0, 0, 0, 0, 0, 0, 0},
                 {0, 1, 1, 1, 1, 0, 1, 1, 1, 0},
@@ -123,7 +200,10 @@ public class Maze {
                 {0, 1, 1, 0, 1, 1, 0, 1, 1, 0},
                 {0, 1, 1, 0, 1, 1, 0, 1, 1, 0},
                 {0, 0, 0, 0, 0, 0, 0, 0, 1, 1}};
-        Maze geerid = new Maze(maze.length, maze[0].length, maze);
+
+        //int[][] maze = generateMaze(30, .23);           //RANDOMLY GENERATE MAZE (comment out to run hardcoded)
+        //int[][] maze = generateMazeNatural(30, .5, 50); //MAZE LOOKS MORE NATURAL (comment out to run pure random)
+        Maze geerid = new Maze(maze.length, maze[0].length, maze); //Switch
         geerid.draw();
         geerid.findPath(0, 0);
         geerid.draw();
