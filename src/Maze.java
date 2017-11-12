ae5960388be2cf4b2e78ed1224083f8468b25217
Same Basket <30935970+sambaskin@users.noreply.github.com>
2017-11-12 16:15:32 -0500

    Maze Submission
    Uploaded in incorrect place last time

--- src/Maze.java
+++ src/Maze.java
@@ -1,9 +1,18 @@
-public class Maze
-{
-    private Cell[][] board;
-    private final int DELAY = 200;
+import sun.plugin2.util.ColorUtil;
+//In main Args: Enter maze, mazeGen, or mazeGenNatural
+import java.awt.*;
 
-    public Maze(int rows, int cols, int[][] map){
+public class Maze {
+    private Cell[][] board;
+    private final int DELAY = 2;
+
+
+    private int prevCol = 0;    //Previous column modified for reverting color
+    private int prevRow = 0;    //Previous row modified for reverting color
+
+    Color cellMarker = new Color(150, 0, 90);  //New color to mark cells
+
+    public Maze(int rows, int cols, int[][] map) {
         StdDraw.setXscale(0, cols);
         StdDraw.setYscale(0, rows);
         board = new Cell[rows][cols];
@@ -11,91 +20,191 @@ public class Maze
         int height = board.length - 1;
         for (int r = 0; r < rows; r++)
             for (int c = 0; c < cols; c++) {
-                board[r][c] = map[r][c] == 1 ? new Cell(c , height - r, 0.5, false) : new Cell(c, height - r, 0.5, true);
+                board[r][c] = map[r][c] == 1 ? new Cell(c, height - r, 0.5, false) : new Cell(c, height - r, 0.5, true);
             }
     }
 
-    public void draw()
-    {
+    public static int[][] generateMaze(int squareLength, double p) {
+        int[][] newMaze = new int[squareLength][squareLength];
+
+        for (int r = 0; r < newMaze.length; r++) {
+            for (int c = 0; c < newMaze.length; c++) {
+                double rand = Math.random();
+                if (r == newMaze.length - 1 && c == newMaze.length - 1 || r == newMaze.length - 1 && c == newMaze.length - 2
+                        || r == 0 && c == 0 || r == 0 && c == 1) //Checks if cell is start or exit
+                {
+                    newMaze[r][c] = 1;
+                } else if (r == 0 || c == 0 || r == newMaze.length - 1 || c == newMaze.length - 1) //Check if cell is border
+                {
+                    newMaze[r][c] = 0;
+                } else {
+                    if (rand > p)
+                        newMaze[r][c] = 1;
+                    else
+                        newMaze[r][c] = 0;
+                }
+            }
+        }
+        return newMaze;
+    }
+
+
+    public static int[][] generateMazeNatural(int squareLength, double p, int strands) {
+        int[][] newMaze = new int[squareLength][squareLength];
+        {
+            int direction = 0; //0-left 1-up 2-right 3-down
+            for (int r = 0; r < newMaze.length; r++) {      //Loop fills in borders with walls and the rest with open
+                for (int c = 0; c < newMaze.length; c++) {
+                    if (r == 0 || c == 0 || r == newMaze.length - 1 || c == newMaze.length - 1) {
+                        newMaze[r][c] = 0;
+                    } else {
+                        newMaze[r][c] = 1;
+                    }
+                    if (r == newMaze.length - 1 && c == newMaze.length - 1 || r == newMaze.length - 1 && c == newMaze.length - 2
+                            || r == 0 && c == 0 || r == 0 && c == 1) //Checks if cell is start or exit
+                    {
+                        newMaze[r][c] = 1;
+                    }
+
+                }
+            }
+
+            while (strands > 0) {
+                strands--;
+
+                int timer = (int) (Math.random() * squareLength*p);
+                int r = (int) (Math.random() * squareLength);
+                int c = (int) (Math.random() * squareLength);
+                while (timer > 0) {
+                    double changeDir = Math.random();
+                    if (changeDir > .75) {
+                        direction = (int) (4 * Math.random());
+                    }
+                    //random start. Moves in direction and changes. random length
+                    double rand = Math.random();
+                    if (r > 0 && c > 0 && r < squareLength - 1 && c < squareLength - 1) {
+                        newMaze[r][c] = 0;
+                        if (direction == 0)
+                            r++;
+                        if (direction == 1)
+                            r--;
+                        if (direction == 2)
+                            c++;
+                        if (direction == 3)
+                            c--;
+
+                    }
+                    timer--;
+                }
+            }
+
+        }
+
+        return newMaze;
+    }
+
+
+    public void draw() {
         for (int r = 0; r < board.length; r++)
-            for (int c = 0; c < board[r].length; c++){
+            for (int c = 0; c < board[r].length; c++) {
                 Cell cell = board[r][c];
                 StdDraw.setPenColor(cell.getColor());
                 StdDraw.filledSquare(cell.getX(), cell.getY(), cell.getRadius());
             }
-            StdDraw.show();
+        StdDraw.show();
     }
 
-    public boolean findPath(int row, int col)
-    {
+    public boolean findPath(int row, int col) {
         boolean isFinished = false;
-
-        //ensure that we can explore this cell
-        if (isValid(row, col))
-        {
-            //mark the cell as visisted
+        if (isValid(row, col)) {
             board[row][col].visitCell();
+            board[prevRow][prevCol].setColor(Color.RED);
+            board[row][col].setColor(cellMarker);
+            prevRow = row;
+            prevCol = col;
             this.draw();
             StdDraw.pause(DELAY);
 
-            //are we at the end of the maze?
-            if (isExit(row, col))
+            if (isExit(row, col)) {
+
+                board[row][col].becomePath();
                 isFinished = true;
 
-            //we are not the end, so let's explore the 4 adjacent cells
-            else{
-                //try the cell below
-                isFinished = findPath(row + 1, col);
-
-                //if not finished yet, try to the right
-                if (!isFinished)
-                    isFinished = findPath(row, col + 1);
-                //if not finished yet, try above
-                if (!isFinished)
-                    isFinished = findPath((row - 1), col);
-                //if not finished yet, try to the left
-                if (!isFinished)
-                    isFinished = findPath((row), col - 1);
             }
 
-            //if we have reached the end, this current cell is part of the final path
-            if (isFinished)
-                board[row][col].becomePath();
-            this.draw();
-            StdDraw.pause(DELAY);
+            if (!isFinished) {
 
-            return isFinished;
+
+                if (!isFinished) {
+
+                    isFinished = findPath(row, col + 1);
+                    System.out.println("3");
+                }
+                if (!isFinished) {
+
+                    isFinished = findPath(row + 1, col);
+                    System.out.println("1");
+                }
+                if (!isFinished) {
+
+                    isFinished = findPath(row, col - 1);
+                    System.out.println("4");
+                }
+
+
+                if (!isFinished) {
+
+                    isFinished = findPath(row - 1, col);
+                    System.out.println("2");
+                }
+
+
+                if (isFinished) {
+                    board[row][col].becomePath();
+                }
+
+
+            }
+
         }
+
         return isFinished;
     }
 
-    private boolean isValid(int row, int col)
-    {
-        //ensure that (row, col) is a valid location in grid
-        if (row >= 0 && row < board.length && col >= 0 && col < board[row].length)
-            //ensure that the cell is open and hasn't been visited yet
-            return(!board[row][col].isWall() && !board[row][col].isVisited());
+
+    private boolean isValid(int row, int col) {
+        if (row >= 0 && col >= 0 && row < board.length && col < board[0].length && board[row][col].isWall() == false
+                && !board[row][col].isVisited())
+            return true;
+
         return false;
     }
 
-    private boolean isExit(int row, int col)
-    {
-        return row == board.length - 1 && col == board[row].length - 1;
+    private boolean isExit(int row, int col) {
+        if (row == board.length - 1 && col == board[0].length - 1) {
+            return true;
+        }
+        return false;
     }
 
     public static void main(String[] args) {
+
         StdDraw.enableDoubleBuffering();
-        int[][] maze = {{1,1,0,0,0,0,0,0,0,0},
-                        {0,1,1,1,1,0,1,1,1,0},
-                        {0,1,1,1,1,0,1,1,0,0},
-                        {0,1,0,1,1,1,1,1,1,0},
-                        {0,1,0,0,0,0,0,1,1,0},
-                        {0,1,1,0,1,1,1,1,1,0},
-                        {0,0,1,0,0,1,0,1,0,0},
-                        {0,1,1,0,1,1,0,1,1,0},
-                        {0,1,1,0,1,1,0,1,1,0},
-                        {0,0,0,0,0,0,0,0,1,1}};
-        Maze geerid = new Maze(maze.length, maze[0].length, maze);
+//        int[][] maze = {{1, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 1, 1, 1, 1, 0, 1, 1, 1, 0},
+//                {0, 1, 1, 1, 1, 0, 1, 1, 0, 0},
+//                {0, 1, 0, 1, 1, 1, 1, 1, 1, 1},
+//                {0, 1, 0, 0, 0, 0, 0, 1, 1, 1},
+//                {0, 1, 1, 0, 1, 1, 1, 1, 1, 1},
+//                {0, 0, 1, 0, 0, 1, 0, 1, 0, 0},
+//                {0, 1, 1, 0, 1, 1, 0, 1, 1, 0},
+//                {0, 1, 1, 0, 1, 1, 0, 1, 1, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 0, 1, 1}};
+
+        //int[][] maze = generateMaze(30, .23);           //RANDOMLY GENERATE MAZE (comment out to run hardcoded)
+        int[][] maze = generateMazeNatural(500, 0.3,4000); //MAZE LOOKS MORE NATURAL (comment out to run pure random)
+        //P changes how long each strand is, strands determines how many strands there are
+        Maze geerid = new Maze(maze.length, maze[0].length, maze); //Switch
         geerid.draw();
         geerid.findPath(0, 0);
         geerid.draw();
